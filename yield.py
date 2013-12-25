45c8a45e80db7b9d31e71425d59355037a7ec461
Tiramisu Mokka <kofemann@gmail.com>
2013-12-25 20:44:57 +0100

    added plot

--- yield.py
+++ yield.py
@@ -20,3 +20,24 @@ activity_per_hour = SUM*N_A*XS_MB/TARGET_A*mkA/CI_in_BQ * DECAY
 
 print "Special Activity              : %.4e Ci/mkA*h" % activity_per_hour
 print "Yield in %2dh, current %4dmkA : %.4e Ci" % (TIME_IN_HOURS, CURRENT, activity_per_hour*CURRENT*TIME_IN_HOURS)
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+YY = (xs_array / loses_array)
+y = []
+S = 0
+for s in YY:
+    n = s* N_A*XS_MB/TARGET_A*mkA/CI_in_BQ * DECAY
+    S = S + n
+    y.append(S)
+
+fig, ax = plt.subplots()
+plt.xlabel('Proton Energy, MeV')
+plt.ylabel('Yield, mCi/C')
+plt.title('%s production yield' % ISOTOPE)
+ax.plot(arange(len(y)), array(y))
+ax.xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, y: x + E_MIN_IN_MEV))
+ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, y: x *1000))
+plt.show()
+
+
